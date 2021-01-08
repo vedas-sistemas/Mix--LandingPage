92229cc68d73d701dc5f9eb23138b071accc8613
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-01-08 17:38:08 -0400

    lint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
+    browser: true
   },
   extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended"
   ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {},
-}
+  rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": "error"
+     "endOfLine": "auto"
+
+  },
+  globals: {
+    $nuxt: true
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
+  
+};
