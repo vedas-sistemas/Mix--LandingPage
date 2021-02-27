817bac8ac9e7ed3d6d491d6c4730ac912db08855
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-02-27 16:41:28 -0400

    plans pricing cards

--- components/Card/CardPlan.vue
+++ components/Card/CardPlan.vue
@@ -14,7 +14,6 @@
         v-for="(item, index) in plans"
         :key="index"
         class="card col-xs-12 col-lg-3 col-md-3"
-        :class="index === 1 ? 'card-contrast' : ''"
       >
         <div class="card-image">
           <img :src="item.image" />
@@ -35,8 +34,8 @@
             </ul>
           </div>
         </div>
-        <div class="card-btn my-3 text-center">
-          <a href="#" v-text="item.button"></a>
+        <div class="my-3 text-center">
+          <button class="btn ripple btn-primary" v-text="item.button"></button>
         </div>
       </div>
     </div>
@@ -104,126 +103,5 @@ export default {
   max-width: 800px;
   color: #6b6b6b;
   margin: 10px auto;
-}
-
-.card {
-  margin: 15px 0px;
-  padding: 30px;
-  border: 1px solid #eff2f6;
-  text-decoration: none;
-  background: #ffffff;
-  transition: all 0.35s ease;
-  flex-shrink: 0;
-  flex-grow: 1;
-  border-radius: 20px 20px;
-  margin: 0 10px;
-}
-
-.card-body {
-  margin: 20px 0px;
-}
-.card .card-body .card-subtitle {
-  font-size: 15px;
-  line-height: 26px;
-  color: #6b6b6b;
-  text-align: center;
-}
-.card .card-text .mdi {
-  font-size: 20px;
-  padding: 0px 10px;
-}
-.card .card-body .card-text {
-  line-height: 20px;
-  color: #6b6b6b;
-  text-align: center;
-  margin: 30px 0px;
-}
-.card ul li {
-  list-style: none;
-}
-.card img {
-  width: 90px;
-}
-.card .card-link {
-  text-decoration: none;
-  text-align: center;
-}
-.card .card-btn a {
-  text-align: center;
-  text-decoration: none;
-  background: #0b10fd;
-  color: #fff;
-  box-shadow: none;
-  border-radius: 8px;
-  font-size: 16px;
-  width: 100%;
-  display: block;
-  font-weight: 800;
-  padding: 18px 42px;
-}
-.card-btn {
-  margin: 15px 0px;
-}
-
-.card-title {
-  font-style: normal;
-  font-weight: bold;
-  font-size: 18px;
-  line-height: 22px;
-  color: #0b10fd;
-  text-align: center;
-}
-.list .list-item {
-  font-size: 15px;
-  text-align: left;
-  display: flex;
-  align-items: center;
-  margin: 15px 0px;
-}
-.card-contrast {
-  border: 0;
-  background: linear-gradient(-45deg, #4144e8 20%, #9300dc 70%);
-}
-.card-contrast .card-body .card-subtitle {
-  font-size: 16px;
-  line-height: 26px;
-  color: #ffff;
-  text-align: center;
-}
-.card-contrast .card-title {
-  font-style: normal;
-  font-weight: bold;
-  font-size: 18px;
-  line-height: 22px;
-  color: #ffff;
-  text-align: center;
-}
-.card-contrast .card-btn a {
-  text-align: center;
-  text-decoration: none;
-  background: #fff;
-  color: #0b10fd;
-  box-shadow: black;
-  border-radius: 8px;
-  font-size: 16px;
-  font-weight: 800;
-  padding: 15px 52px;
-  width: 100%;
-}
-.card-contrast .card-body .card-text {
-  line-height: 20px;
-  color: #ffff;
-  text-align: center;
-}
-.card-contrast .card-link {
-  text-decoration: none;
-  color: #0b10fd;
-}
-.card-footer {
-  padding: 20px 0px;
-}
-.card-image {
-  margin: 0 auto;
-  width: 90px;
 }
 </style>
