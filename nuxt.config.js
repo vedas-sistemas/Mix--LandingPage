fb938c8c2dd7c3a2e69575e1cf4780d7d940563d
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-03-03 16:51:52 -0400

    dependencies configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    'nuxt-gsap-module',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
