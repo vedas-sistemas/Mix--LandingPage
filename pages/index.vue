ffe45cd11a70963fa63974947ca21149557c346f
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-03-03 17:01:14 -0400

    images illustration pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <Header />
-    <Feature />
-    <CardPlan />
-    <DetailsApp />
-    <Categorie />
-    <Test />
+    <div class="col-ml-12 col-lg-7 mx-auto">
+      <Header />
+    </div>
+    <div class="col-ml-12 col-lg-7 mx-auto">
+      <Categorie />
+    </div>
+
+    <div class="bg-theme">
+      <DetailsApp />
+    </div>
+    <div class="col-ml-12 col-lg-7 mx-auto">
+      <Feature />
+    </div>
+    <div class="col-ml-12 col-lg-7 mx-auto">
+      <CardPlan />
+    </div>
   </div>
 </template>
 <script></script>
