92a7b2218d8fbdf568eaaec1b064681ff86c61cd
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-03-03 17:03:48 -0400

    loading pages

---
+++ pages/loading.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="row colunm fill-height"></div>
+</template>
+
+<script>
+import { AtomSpinner } from 'epic-spinners'
+
+export default {
+  layout: 'loading',
+  components: {
+    AtomSpinner,
+  },
+  data() {
+    return {
+      firstImage: require('~/assets/images/register/first.svg'),
+    }
+  },
+  methods: {
+    next() {
+      console.log('next')
+      this.$router.push({ path: '/' })
+    },
+  },
+}
+</script>
+
+<style>
+.info-container {
+  background: #6628ea;
+  width: 100%;
+  padding-top: 60px;
+  color: #d7e9ff;
+  display: flex;
+  justify-content: center;
+}
+
+.info-content {
+  width: 70%;
+}
+
+.info-title {
+  font-size: 120%;
+  width: 70%;
+}
+
+.info-text {
+  margin-top: 15px;
+  margin-bottom: 20px;
+  font-size: 18px;
+}
+</style>
