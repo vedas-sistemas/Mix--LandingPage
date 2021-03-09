110c64754770543862361da40e40d17d6a076c25
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-03-08 20:01:09 -0400

    nuxt prev

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,6 @@ export default {
     port: 8000, // default: 3000
     host: '0.0.0.0', // default: localhost
   }, // other configs
-  target: 'static',
-
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     htmlAttrs: {
