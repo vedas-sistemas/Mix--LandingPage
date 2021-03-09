b191a649e607cbb25b5ba3c249839866f600fc9a
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-03-08 20:19:11 -0400

    bug fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
-    'nuxtjs/mdi-font',
+    'nuxtjs-mdi-font',
     '@neneos/nuxt-animate.css',
     'vue-scrollto/nuxt',
   ],
