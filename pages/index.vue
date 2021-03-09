89a449d79837325f65385b33377d19f5692d00d2
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-03-08 20:47:48 -0400

    monitor

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <div class="col-ml-12 col-lg-7 mx-auto">
+    <div class="col-ml-12 col-lg-12 mx-auto">
       <Header />
     </div>
-    <div class="col-ml-12 col-lg-7 mx-auto">
+    <div class="col-ml-12 col-lg-12 mx-auto">
       <Categorie />
     </div>
 
     <div class="bg-theme">
       <DetailsApp />
     </div>
-    <div class="col-ml-12 col-lg-7 mx-auto">
+    <div class="col-ml-12 col-lg-12 mx-auto">
       <Feature />
     </div>
-    <div class="col-ml-12 col-lg-7 mx-auto">
+    <div class="col-ml-12 col-lg-12 mx-auto">
       <CardPlan />
     </div>
   </div>
